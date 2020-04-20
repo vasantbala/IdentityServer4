38b79e4a2253f74771daa9304d00df88ca0691ae
Vasant Balasubramanian <vasantbala@outlook.com>
2020-04-20 16:34:55 -0500

    Vanilla legacy webapi resource server for Multi Bearer token experiment

---
+++ IdentityServer4.Learning/LegacyResourceServerMB/Controllers/FooController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LegacyResourceServerMB.Controllers
+{
+    public class FooController : ApiController
+    {
+		[HttpGet]
+		public string Get()
+		{
+			return "Foo";
+		}
+    }
+}
